9a6e0dda5669f2d262d592b7ff0121320e9c0f01
abhixyz1 <abhinayazrf@gmail.com>
2025-10-14 18:05:44 +0700

    fixing config

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,6 @@ const nextConfig: NextConfig = {
   images: { unoptimized: true }, 
   basePath: isProd ? `/${repo}` : '',
   assetPrefix: isProd ? `/${repo}/` : '',
+  trailingSlash: true,
 }
-
 export default nextConfig
